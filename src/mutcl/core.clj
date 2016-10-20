6f5cf64e2d2422fc69281701411d133288cfaa52
David Shaked <ds2643@columbia.edu>
2016-10-20 14:29:45 -0400

    backup

--- src/mutcl/core.clj
+++ src/mutcl/core.clj
@@ -2,7 +2,10 @@
   (:gen-class)
   (:require [clojure.zip :as z]
             [clojure.string :as str]
-            [mutcl.filehandling :as fh]))
+            [me.raynes.fs :as fs]
+            [clojure.java.io :as io]
+            [clojure.edn :as edn])
+  (:import [java.io PushbackReader]))
 
 (defn stochastic-tree-f-app [f tree]
   "apply function to an arbitrary node of an unbalanced, asymmetric tree, with equal probability of application of the function to all nodes in the tree"
@@ -38,8 +41,7 @@
         is-set (cloj-form-sets :is)
         eo-set (cloj-form-sets :eo)
         cnd-set (cloj-form-sets :cnd)
-        another-one (fn [ix sx] (rand-nth (filter (partial not= ix) (seq sx))))
-        ]
+        another-one (fn [ix sx] (rand-nth (filter (partial not= ix) (seq sx))))]
   (cond
     (contains? art-set item) (another-one item art-set)
     (contains? lgc-set item) (another-one item lgc-set)
@@ -57,6 +59,102 @@
   "given a Clojure program, return a copy(?) of the program for which a single node in the ast has been substituted with some substitution function"
   [f b]
   (stochastic-tree-f-app f b))
+
+(defn read-forms
+  "read clojure src as programmic data"
+  [file]
+  (let [rdr (-> file io/file io/reader PushbackReader.)
+        sentinel (Object.)]
+    (loop [forms []]
+      (let [form (edn/read {:eof sentinel} rdr)]
+        (if (= sentinel form)
+          forms
+          (recur (conj forms form)))))))
+
+(defn copy-file                                      ;; OK
+  "copies a directory at specified path to destination path"
+  [src-path dest-path]
+  (fs/copy-dir src-path dest-path))
+
+(defn overwrite-file
+  "writes content to disk in place of specified file"
+  [file-name cont-as-str]
+  {:post (string? (slurp file-name))}
+  (do
+    (io/delete-file file-name)
+    (spit file-name cont-as-str)))
+
+(defn project-src-files                             ;; OK
+  "return list of src file contents"
+  [project-name]
+  (filter #(and (.endsWith (.getName %) ".clj")
+                 (not (.startsWith (.getName %) ".")))
+          (file-seq (io/file (str project-name "/src/" project-name)))))
+
+(defn src-dir                                     ;; OK
+ "returns lein project's source file directory object"
+  [project-name clone-name]
+  (io/file (str clone-name "/src/" project-name)))
+
+(defn choose-rand-src-file
+  [src-dir]
+  (rand-nth src-dir))
+
+(defn delete-mutant
+  [file-name]
+  (io/delete-file file-name))
+
+(defn make-dir                ;; OK
+  "create a directory"
+  [name]
+  {:pre [(string? name)]}
+  (.mkdir (io/file name)))
+
+(defn delete-file-recur       ;; OK
+  "delete file recursively"
+  [file-name]
+  (let [func (fn [func f]
+               (when (.isDirectory f)
+                 (doseq [f2 (.listFiles f)]
+                   (func func f2)))
+               (io/delete-file f))]
+    (func func (io/file file-name))))
+
+(defn create-src-backup
+  "create souce backup for later reference"
+  [src-temp-path src-obj-path]
+  (do
+    (make-dir src-temp-name)
+    (copy-file src-obj-path src-temp-path))
+
+(defn refresh-src
+  [src-dir back-up-dir]
+  (do
+    (delete-file-recur src-dir)
+    (io/copy back-up-dir src-dir)))
+
+(defn i-shell
+  "programmatic imperative shell that executes entire mutation and data generation process
+  n -> iterations"
+  [proj-path n]
+  (let [proj-obj (fs/absolute proj-path)
+        prog-name (fs/name proj-obj)
+        clone-name (str proj-name "_clone")] ;; TODO: guarantee avoiding naming conflict
+    (do
+        (copy-file proj-path clone-name) ;; copy project; original proj file spared of modification; all mutations performed on a clone
+        (let [clone-obj (fs/absolute clone-name)
+              clone-src-obj (src-dir proj-name clone-name)
+              src-temp-name "src_temp"]
+          (do
+            (create-src-backup src-temp-name clone-src-obj)
+            (for [x (range n)]
+              (println "foo")
+              ;; mutate file contents
+              ;; run tests
+              ;; output data
+              (refresh-src clone-src-obj src-temp-name))
+            (delete-file-recur src-temp-name)
+            (delete-file-recur clone-obj))))))
 
 (defn -main
   [& args]
