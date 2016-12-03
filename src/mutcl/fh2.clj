2d50954a13524ff6644e30bd8bddd922d6c60ae1
David Shaked <ds2643@columbia.edu>
2016-12-03 13:30:22 -0500

    fh2 updates

--- src/mutcl/fh2.clj
+++ src/mutcl/fh2.clj
@@ -7,20 +7,6 @@
   (:use [mutcl.alg :refer :all])
   (:import [java.io PushbackReader]))
 
-;; fh(1) functions:
-;;   mutate-branch
-;;   read-forms
-;;   mutate-src-file
-;;   project-src-files
-;;   src-dir
-;;   choose-rand-src-file
-;;   delete-mutant
-;;   make-dir
-;;   delete-file-recur
-;;   create-src-backup
-;;   refresh-src
-;;   ishell
-
 (defn gen-file-name
   "generate an name for a file in the specified directory such that no name space conflicts can occur"
   [dir]
@@ -49,12 +35,34 @@
   {:pre [(fs/exists? file-object)]}
   (fs/delete-dir file-object))
 
+(comment
+
+;;
+;;  salvage:
+;;
+
+(defn project-src-files                             ;; OK
+  "return list of src file contents"
+  [project-name]
+  (filter #(and (.endsWith (.getName %) ".clj")
+                 not (.startsWith (.getName %) ".")))
+          (file-seq (io/file (str project-name "/src/" project-name))))
+
+(defn src-dir                                     ;; OK
+ "returns lein project's source file directory object"
+  [project-name clone-name]
+  (io/file (str clone-name "/src/" project-name)))
+
+(defn choose-rand-src-file
+  [src-dir]
+  (rand-nth src-dir))
+;;
+
 (defn src-copy
   "given reference to project file object, returns reference to source file-object"
   [file-object]
-  {}
   {:post [(fs/childof? file-object src)]}
-  (let [src (fs/absolute ())]))
+  (let [src (fs/absolute ())])))
 
 (comment ;; TODO implement these functions
 (defn replace-src "replace mutated src file contents" [src-file-object dst-file-object] nil)
